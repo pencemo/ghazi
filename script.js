c749c74a209644eae340780a9f6cad65319c4d3c
unknown <mnmsby4@gmail.com>
2024-02-05 17:42:21 +0530

    new file added

--- script.js
+++ script.js
@@ -127,109 +127,145 @@ function googleTranslateElementInit() {
 
 
 
-const images =[
-  Flooring={
-      img1 : "./images/img-1.jpg",
-      img2 : "./images/img-2.jpg",
-      img3 : "./images/img-3.jpg",
-      img4 : "./images/img-10.jpg",
-      img5 : "./images/img-12.jpg",
-      img6 : "./images/img-11.jpg",
-      img7 : "./images/img-7.jpg",
-  },
-  Furniture={
-      img1 : "./images/123.png",
-      img2 : "./images/img-41.jpg",
-      img3 : "./images/landbgmain.jpg",
-      img4 : "./images/627.png",
-      img5 : "./images/landmarks-modern-city.jpg",
-      img6 : "./images/img-29.jpg",
-      img7 : "./images/img-30.jpg",
-  },
-  Curtains={
-      img1 : "./images/img-20.jpg",
-      img2 : "./images/img-21.jpg",
-      img3 : "./images/img-22.jpg",
-      img4 : "./images/img-23.jpg",
-      img5 : "./images/img-24.jpg",
-      img6 : "./images/img-25.jpg",
-      img7 : "./images/img-26.jpg",
-  },
-  Wallpaper={
-      img1 : "./images/img-31.jpg",
-      img2 : "./images/img-32.jpg",
-      img3 : "./images/img-33.jpg",
-      img4 : "./images/img-34.webp",
-      img5 : "./images/img-35.webp",
-      img6 : "./images/img-36.webp",
-      img7 : "./images/img-37.webp",
-  },
-];
+// const images =[
+//   Flooring={
+//       img1 : "./images/img-1.jpg",
+//       img2 : "./images/img-2.jpg",
+//       img3 : "./images/img-3.jpg",
+//       img4 : "./images/img-10.jpg",
+//       img5 : "./images/img-12.jpg",
+//       img6 : "./images/img-11.jpg",
+//       img7 : "./images/img-7.jpg",
+//   },
+//   Furniture={
+//       img1 : "./images/123.png",
+//       img2 : "./images/img-41.jpg",
+//       img3 : "./images/landbgmain.jpg",
+//       img4 : "./images/627.png",
+//       img5 : "./images/landmarks-modern-city.jpg",
+//       img6 : "./images/img-29.jpg",
+//       img7 : "./images/img-30.jpg",
+//   },
+//   Curtains={
+//       img1 : "./images/img-20.jpg",
+//       img2 : "./images/img-21.jpg",
+//       img3 : "./images/img-22.jpg",
+//       img4 : "./images/img-23.jpg",
+//       img5 : "./images/img-24.jpg",
+//       img6 : "./images/img-25.jpg",
+//       img7 : "./images/img-26.jpg",
+//   },
+//   Wallpaper={
+//       img1 : "./images/img-31.jpg",
+//       img2 : "./images/img-32.jpg",
+//       img3 : "./images/img-33.jpg",
+//       img4 : "./images/img-34.webp",
+//       img5 : "./images/img-35.webp",
+//       img6 : "./images/img-36.webp",
+//       img7 : "./images/img-37.webp",
+//   },
+// ];
 
 
-var close_btn=document.querySelector(".close-img");
-var model_bg=document.querySelector(".model-background");
+// var close_btn=document.querySelector(".close-img");
+// var model_bg=document.querySelector(".model-background");
 
 
-close_btn.addEventListener("click", model_close);
-model_bg.addEventListener("click", model_close)
+// close_btn.addEventListener("click", model_close);
+// model_bg.addEventListener("click", model_close)
 
-document.querySelector(".open-btn-1").addEventListener("click", model_open_1);
+// document.querySelector(".open-btn-1").addEventListener("click", model_open_1);
 
-function model_open_1() {
-  document.querySelector(".model-img-1").src=Flooring.img1
-  document.querySelector(".model-img-2").src=Flooring.img2
-  document.querySelector(".model-img-3").src=Flooring.img3
-  document.querySelector(".model-img-4").src=Flooring.img4
-  document.querySelector(".model-img-5").src=Flooring.img5
-  document.querySelector(".model-img-6").src=Flooring.img6
-  document.querySelector(".model-img-7").src=Flooring.img7
-  document.querySelector(".model").classList.add("model-active")
+// function model_open_1() {
+//   document.querySelector(".model-img-1").src=Flooring.img1
+//   document.querySelector(".model-img-2").src=Flooring.img2
+//   document.querySelector(".model-img-3").src=Flooring.img3
+//   document.querySelector(".model-img-4").src=Flooring.img4
+//   document.querySelector(".model-img-5").src=Flooring.img5
+//   document.querySelector(".model-img-6").src=Flooring.img6
+//   document.querySelector(".model-img-7").src=Flooring.img7
+//   document.querySelector(".model").classList.add("model-active")
+// }
+
+// document.querySelector(".open-btn-2").addEventListener("click", model_open_2);
+
+// function model_open_2() {
+//   document.querySelector(".model-img-1").src=Furniture.img1
+//   document.querySelector(".model-img-2").src=Furniture.img2
+//   document.querySelector(".model-img-3").src=Furniture.img3
+//   document.querySelector(".model-img-4").src=Furniture.img4
+//   document.querySelector(".model-img-5").src=Furniture.img5
+//   document.querySelector(".model-img-6").src=Furniture.img6
+//   document.querySelector(".model-img-7").src=Furniture.img7
+//   document.querySelector(".model").classList.add("model-active")
+// }
+
+// document.querySelector(".open-btn-3").addEventListener("click", model_open_3);
+
+// function model_open_3() {
+//   document.querySelector(".model-img-1").src=Curtains.img1
+//   document.querySelector(".model-img-2").src=Curtains.img2
+//   document.querySelector(".model-img-3").src=Curtains.img3
+//   document.querySelector(".model-img-4").src=Curtains.img4
+//   document.querySelector(".model-img-5").src=Curtains.img5
+//   document.querySelector(".model-img-6").src=Curtains.img6
+//   document.querySelector(".model-img-7").src=Curtains.img7
+//   document.querySelector(".model").classList.add("model-active")
+// }
+
+// document.querySelector(".open-btn-4").addEventListener("click", model_open_4);
+
+// function model_open_4() {
+//   document.querySelector(".model-img-1").src=Wallpaper.img1
+//   document.querySelector(".model-img-2").src=Wallpaper.img2
+//   document.querySelector(".model-img-3").src=Wallpaper.img3
+//   document.querySelector(".model-img-4").src=Wallpaper.img4
+//   document.querySelector(".model-img-5").src=Wallpaper.img5
+//   document.querySelector(".model-img-6").src=Wallpaper.img6
+//   document.querySelector(".model-img-7").src=Wallpaper.img7
+//   document.querySelector(".model").classList.add("model-active")
+// }
+
+
+
+var open_btn=document.querySelector(".open-btn-1");
+
+open_btn.addEventListener("click", model_open);
+
+function model_open() {
+  document.querySelector(".model-1").classList.add("model-active")
 }
 
-document.querySelector(".open-btn-2").addEventListener("click", model_open_2);
+document.querySelector(".open-btn-2").addEventListener("click", model_open2);
 
-function model_open_2() {
-  document.querySelector(".model-img-1").src=Furniture.img1
-  document.querySelector(".model-img-2").src=Furniture.img2
-  document.querySelector(".model-img-3").src=Furniture.img3
-  document.querySelector(".model-img-4").src=Furniture.img4
-  document.querySelector(".model-img-5").src=Furniture.img5
-  document.querySelector(".model-img-6").src=Furniture.img6
-  document.querySelector(".model-img-7").src=Furniture.img7
-  document.querySelector(".model").classList.add("model-active")
-}
-
-document.querySelector(".open-btn-3").addEventListener("click", model_open_3);
-
-function model_open_3() {
-  document.querySelector(".model-img-1").src=Curtains.img1
-  document.querySelector(".model-img-2").src=Curtains.img2
-  document.querySelector(".model-img-3").src=Curtains.img3
-  document.querySelector(".model-img-4").src=Curtains.img4
-  document.querySelector(".model-img-5").src=Curtains.img5
-  document.querySelector(".model-img-6").src=Curtains.img6
-  document.querySelector(".model-img-7").src=Curtains.img7
-  document.querySelector(".model").classList.add("model-active")
-}
-
-document.querySelector(".open-btn-4").addEventListener("click", model_open_4);
-
-function model_open_4() {
-  document.querySelector(".model-img-1").src=Wallpaper.img1
-  document.querySelector(".model-img-2").src=Wallpaper.img2
-  document.querySelector(".model-img-3").src=Wallpaper.img3
-  document.querySelector(".model-img-4").src=Wallpaper.img4
-  document.querySelector(".model-img-5").src=Wallpaper.img5
-  document.querySelector(".model-img-6").src=Wallpaper.img6
-  document.querySelector(".model-img-7").src=Wallpaper.img7
-  document.querySelector(".model").classList.add("model-active")
+function model_open2() {
+  document.querySelector(".model-2").classList.add("model-active")
 }
 
 
 
 
+
+
+
+
+document.querySelector(".bg-1").addEventListener("click", model_close);
+document.querySelector(".close-1").addEventListener("click", model_close);
+
+document.querySelector(".bg-2").addEventListener("click", model_close);
+document.querySelector(".close-2").addEventListener("click", model_close);
+
+document.querySelector(".bg-3").addEventListener("click", model_close);
+document.querySelector(".close-3").addEventListener("click", model_close);
+
+document.querySelector(".bg-4").addEventListener("click", model_close);
+document.querySelector(".close-4").addEventListener("click", model_close);
 
 function model_close() {
   document.querySelector(".model").classList.remove("model-active")
 }
+
+
+
+
+
